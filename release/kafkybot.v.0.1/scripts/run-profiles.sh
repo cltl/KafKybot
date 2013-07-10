44438e1f15fb469c48d6b1bf26a66b5c8fbf59ed
piekvossen <piek.vossen@vu.nl>
2013-07-10 12:00:27 +0200

    - First version of KafKybot in Github-cltl

--- release/kafkybot.v.0.1/scripts/run-profiles.sh
+++ release/kafkybot.v.0.1/scripts/run-profiles.sh
@@ -1,2 +1,2 @@
-java -Xmx812m -cp ../lib/vu.kafkybot.KafKybot-1.0-jar-with-dependencies.jar vu.kafkybot.KafKybot --kaf-file "../example/bus-accident.ont.dep.kaf" --profiles "../profiles/profiles-sem.txt" > "../example/bus-accident.ont.dep.kaf.sem.tpl" --overview
-java -Xmx812m -cp ../lib/vu.kafkybot.KafKybot-1.0-jar-with-dependencies.jar vu.kafkybot.KafKybot --kaf-file "../example/bus-accident.ont.dep.kaf" --profiles "../profiles/profiles-syn.txt" > "../example/bus-accident.ont.dep.kaf.syn.tpl" --overview
+java -Xmx812m -cp ../lib/KafKybot-1.0-jar-with-dependencies.jar vu.kafkybot.KafKybot --kaf-file "../example/bus-accident.ont.dep.kaf" --profiles "../profiles/profiles-sem.txt" --overview > "../example/bus-accident.ont.dep.kaf.sem.tpl"
+java -Xmx812m -cp ../lib/KafKybot-1.0-jar-with-dependencies.jar vu.kafkybot.KafKybot --kaf-file "../example/bus-accident.ont.dep.kaf" --profiles "../profiles/profiles-syn.txt" --overview > "../example/bus-accident.ont.dep.kaf.syn.tpl"
