9d928ad2e91c1f08699a2282a5b598d421fddfd0
piekvossen <piek.vossen@vu.nl>
2014-03-04 08:56:17 +0100

    - Simple Predicate Role classification

--- src/main/java/vu/kafkybot/KafKybotPredicateFolderApi.java
+++ src/main/java/vu/kafkybot/KafKybotPredicateFolderApi.java
@@ -22,7 +22,8 @@ public class KafKybotPredicateFolderApi {
        // String pathToKafFile = "//Tools/kafkybot.v.0.1/cars/Volkswagen-48-c6577c99c0be5cef4793cf54defb55e345fbe965.kaf.step.0.step.1.step.2.step.3.event.kaf";
         //String pathToKafFile = "/Tools/kafkybot.v.0.1/cars2";
         //String pathToProfiles = "/Tools/kafkybot.v.0.1/profiles/car-profiles-dep-all-event-subj-obj-en.txt";
-        String pathToKafFile = "/Projects/NEWSREADER/data/marit/kaf";
+        //String pathToKafFile = "/Projects/NEWSREADER/data/marit/kaf";
+        String pathToKafFile = "/Users/piek/Desktop/Thomese/Thomese_book_opener_nwr";
         String pathToProfiles = "/Tools/kafkybot.v.0.1/profiles/car-profiles-dep-all-event-subj-obj-nl.txt";
         String extension = "event.kaf";
         String format = "NAF";
